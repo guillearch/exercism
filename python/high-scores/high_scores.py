bea93ce7286a557e558c2cc00fb478f862fa2907
guillearch <gcastellano@protonmail.com>
2019-01-22 16:43:38 +0100

    Updated the __init__() method to pre-calculate the values

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,18 +1,21 @@
 class HighScores(object):
     def __init__(self, scores):
         self.scores = scores
+        self.latest_score = self.scores[-1]
+        self.personal_best_score = max(self.scores)
+        self.personal_top_score = sorted(self.scores, reverse=True)[:3]
+        self.dif = self.personal_best() - self.latest()
 
     def latest(self):
-        return self.scores[-1]
+        return self.latest_score
 
     def personal_best(self):
-        return max(self.scores)
+        return self.personal_best_score
 
     def personal_top(self):
-        return sorted(self.scores, reverse=True)[:3]
+        return self.personal_top_score
 
     def report(self):
-        dif = self.personal_best() - self.latest()
-        if dif <= 0:
+        if self.dif <= 0:
             return f"Your latest score was {self.latest()}. That's your personal best!"
-        return f"Your latest score was {self.latest()}. That's {dif} short of your personal best!"
+        return f"Your latest score was {self.latest()}. That's {self.dif} short of your personal best!"
