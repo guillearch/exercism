da41858392d7a598f951dce19346cbff4c61b8eb
guillearch <gcastellano@protonmail.com>
2019-01-28 07:57:13 +0100

    First submission

---
+++ python/matrix/matrix.py
@@ -0,0 +1,11 @@
+class Matrix(object):
+    def __init__(self, matrix_string):
+        self.matrix = [[int(x) for x in row.split()]
+                       for row in matrix_string.splitlines()
+                       ]
+
+    def row(self, index):
+        return self.matrix[index-1]
+
+    def column(self, index):
+        return [row[index-1] for row in self.matrix]
