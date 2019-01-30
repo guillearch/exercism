275068fb68bf451adc0b352a56aba23c2e588a52
guillearch <gcastellano@protonmail.com>
2019-01-30 15:04:44 +0100

    Updated the __init__() method to calculate the rows

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -3,9 +3,12 @@ class Matrix(object):
         self.matrix = [[int(x) for x in row.split()]
                        for row in matrix_string.splitlines()
                        ]
+        self.columns = [[row[i] for row in self.matrix]
+                        for i in range(len(self.matrix[0]))
+                        ]
 
     def row(self, index):
         return self.matrix[index-1]
 
     def column(self, index):
-        return [row[index-1] for row in self.matrix]
+        return self.columns[index-1]
