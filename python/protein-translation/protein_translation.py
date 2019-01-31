1d22331918086fa219e6e5b67344894d26ab8a51
guillearch <gcastellano@protonmail.com>
2019-01-31 07:45:58 +0100

    Improved PEP8 compliance

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,22 +1,22 @@
 def proteins(strand):
 
-    conversion = {'AUG':'Methionine',
-                  'UUU':'Phenylalanine',
-                  'UUC':'Phenylalanine',
-                  'UUA':'Leucine',
-                  'UUG':'Leucine',
-                  'UCU':'Serine',
-                  'UCC':'Serine',
-                  'UCA':'Serine',
-                  'UCG':'Serine',
-                  'UAU':'Tyrosine',
-                  'UAC':'Tyrosine',
-                  'UGU':'Cysteine',
-                  'UGC':'Cysteine',
-                  'UGG':'Tryptophan',
-                  'UAA':'STOP',
-                  'UAG':'STOP',
-                  'UGA':'STOP'
+    conversion = {'AUG': 'Methionine',
+                  'UUU': 'Phenylalanine',
+                  'UUC': 'Phenylalanine',
+                  'UUA': 'Leucine',
+                  'UUG': 'Leucine',
+                  'UCU': 'Serine',
+                  'UCC': 'Serine',
+                  'UCA': 'Serine',
+                  'UCG': 'Serine',
+                  'UAU': 'Tyrosine',
+                  'UAC': 'Tyrosine',
+                  'UGU': 'Cysteine',
+                  'UGC': 'Cysteine',
+                  'UGG': 'Tryptophan',
+                  'UAA': 'STOP',
+                  'UAG': 'STOP',
+                  'UGA': 'STOP'
                   }
 
     codons = [strand[i:i+3] for i in range(0, len(strand), 3)]
