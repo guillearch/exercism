7e1b6e18667f745268ed5da2f3c6be4bbde99cd3
guillearch <gcastellano@protonmail.com>
2019-02-02 09:27:29 +0100

    First submission

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,21 +1,48 @@
 // Package triangle is the solution to: https://github.com/exercism/go/tree/master/exercises/triangle.
 package triangle
 
+import "math"
 
-// Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+// Kind describes the type of triangle.
+type Kind string
 
+// Types of triangle.
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	NaT = "NaT"
+	Equ = "Equ"
+	Iso = "Iso"
+	Sca = "Sca"
 )
 
-// KindFromSides returns .
+// KindFromSides determines if a triangle is equilateral, isosceles or scalene.
 func KindFromSides(a, b, c float64) Kind {
 
 	var k Kind
+
+	if isTriangle(a, b, c) == false {
+		k = NaT
+	} else {
+		if a == b || b == c || c == a {
+			if a == b && b == c {
+				k = Equ
+			} else {
+				k = Iso
+			}
+		} else {
+			k = Sca
+		}
+	}
+
 	return k
+}
+
+func isTriangle(a, b, c float64) bool {
+	if a > 0 && b > 0 && c > 0 {
+		if !(math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0)) {
+			if a+b >= c && b+c >= a && c+a >= b {
+				return true
+			}
+		}
+	}
+	return false
 }
